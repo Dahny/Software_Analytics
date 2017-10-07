808f7e070eac9d1132216d0c2670b00dafd55643
Danny Plenge <dannyplenge@gmail.com>
2017-10-07 20:32:53 +0200

    Nou het werkt, heb initiele data mine gedaan op alle commits van sonarqube als test (duurde anderhalf uur)

--- Artifact_SA/src/main/java/Group_SA/Artifact_SA/RefactorStudy.java
+++ Artifact_SA/src/main/java/Group_SA/Artifact_SA/RefactorStudy.java
@@ -5,6 +5,7 @@ import org.repodriller.RepositoryMining;
 import org.repodriller.Study;
 import org.repodriller.filter.range.Commits;
 import org.repodriller.persistence.csv.CSVFile;
+import org.repodriller.scm.GitRemoteRepository;
 import org.repodriller.scm.GitRepository;
 
 
@@ -17,9 +18,16 @@ public class RefactorStudy implements Study
 	@Override
 	public void execute() {
 		new RepositoryMining()
-			.in(GitRepository.singleProject("/Users/mauricioaniche/Desktop/tutorial/jfreechart-fse"))
+			.in(
+					//GitRepository.singleProject("/Users/mauricioaniche/Desktop/tutorial/jfreechart-fse")
+					GitRemoteRepository
+					.hostedOn("https://github.com/SonarSource/sonarqube.git")
+					.inTempDir("/temp")
+					.asBareRepos()
+					.buildAsSCMRepository()
+					)
 			.through(Commits.all())
-			.process(new DevelopersVisitor(), new CSVFile("/Users/mauricioaniche/Desktop/tutorial/q1.csv"))
+			.process(new DevelopersVisitor(), new CSVFile("/Users/Dplen/Documents/Software_Analytics/Artifact_SA/data.data.csv"))
 			.mine();
 	}
 }
