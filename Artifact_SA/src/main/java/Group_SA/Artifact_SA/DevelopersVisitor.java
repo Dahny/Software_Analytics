45b33bfedfc3f3c691bc86805903facb0408e8ed
Marc Zwart <marc@tamtam.nl>
2017-10-14 13:55:46 +0200

    basis-basis implementatie, kan nu refactors naar file schrijven

--- Artifact_SA/src/main/java/Group_SA/Artifact_SA/DevelopersVisitor.java
+++ Artifact_SA/src/main/java/Group_SA/Artifact_SA/DevelopersVisitor.java
@@ -1,55 +1,95 @@
 package Group_SA.Artifact_SA;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.eclipse.jgit.lib.Repository;
+import org.eclipse.jgit.revwalk.RevCommit;
+import org.refactoringminer.api.GitHistoryRefactoringMiner;
+import org.refactoringminer.api.GitService;
+import org.refactoringminer.api.Refactoring;
+import org.refactoringminer.api.RefactoringHandler;
+import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl;
+import org.refactoringminer.util.GitServiceImpl;
 import org.repodriller.domain.Commit;
 import org.repodriller.domain.Modification;
 import org.repodriller.domain.ModificationType;
 import org.repodriller.persistence.PersistenceMechanism;
 import org.repodriller.scm.CommitVisitor;
 import org.repodriller.scm.SCMRepository;
-
 import com.github.mauricioaniche.ck.CK;
 import com.github.mauricioaniche.ck.CKReport;
 
 public class DevelopersVisitor implements CommitVisitor {
 	
-	static final String repoPath = "C:\\Temp\\MapDB\\"; // C:/temp/...
-	
+	static final String repoPath = "C:\\Temp\\sonarqube\\"; // C:/temp/...
+	public static int refCount = 0;
 	@Override
 	public void process(SCMRepository repo, Commit commit, PersistenceMechanism writer) {
-		CK ck = new CK();
-	    
-		try {
-			repo.getScm().checkout(commit.getHash());
-			for(Modification mod : commit.getModifications()) {
-		      if(mod.getType().equals(ModificationType.MODIFY) && mod.getNewPath().endsWith(".java")) {
-		    	  
-		    	StringBuilder path = new StringBuilder(repoPath); 
-		    	String[] parts = mod.getNewPath().split("/", -1);
-		    	String fileName = parts[parts.length-1];
-		    	parts[parts.length-1] = "";
-		    	for(String part : parts) {
-		    		path.append(part).append("\\");
-		    	}
-		    	path.delete(path.length() - 2, path.length() - 1);
-		    	System.out.println("PATH: " + path);
-		    	CKReport report = ck.calculate(path.toString());
-		    	System.err.println(report.get(path.toString()+fileName));
-		      
-		      }
-		    }
-		} finally {
-			repo.getScm().reset();
-		}
 		
 	    
-	
-		writer.write(	
-			commit.getAuthor().getName(),
-			commit.getAuthor().getEmail()
-		);
+		GitService gitService = new GitServiceImpl();
+		GitHistoryRefactoringMiner miner = new GitHistoryRefactoringMinerImpl();
+		
+		try {
+			Repository refRepo = gitService.cloneIfNotExists("C:/Temp/"+RefactorStudy.repo_name+"_Ref/",RefactorStudy.repository);
+			miner.detectAtCommit(refRepo, null, commit.getHash(), new RefactoringHandler() {
+			  @Override
+			  public void handle(String commitId, List<Refactoring> refactorings) {
+			    StringBuilder refactorsString = new StringBuilder();
+			    boolean anyRefs = false;
+			    for (Refactoring ref : refactorings) {
+			    	refactorsString.append(',').append(ref.toString());
+			    	anyRefs = true;
+			    }
+			    if(anyRefs) {
+			    	refactorsString.delete(0, 1); //remove first comma
+					writer.write(	
+						commit.getAuthor().getName(),
+						commitId,
+						refactorsString.toString()					
+					);
+			    } 
+			  }
+			});
+		} catch(NullPointerException e) {
+			//object was not found on disk just ignore :/
+			e.printStackTrace();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+//		try {
+//			CK ck = new CK();
+//			repo.getScm().checkout(commit.getHash());
+//			for(Modification mod : commit.getModifications()) {
+//		      if(mod.getType().equals(ModificationType.MODIFY) && mod.getNewPath().endsWith(".java")) {
+//		    	  
+//		    	StringBuilder path = new StringBuilder(repoPath); 
+//		    	String[] parts = mod.getNewPath().split("/", -1);
+//		    	String fileName = parts[parts.length-1];
+//		    	parts[parts.length-1] = "";
+//		    	for(String part : parts) {
+//		    		path.append(part).append("\\");
+//		    	}
+//		    	path.delete(path.length() - 2, path.length() - 1);
+//		    	System.out.println("PATH: " + path);
+//		    	CKReport report = ck.calculate(path.toString());
+//		    	System.err.println(report.get(path.toString()+fileName));
+//		      }
+//		    }
+//			
+//		} 
+//		catch(Exception e) {
+//			e.printStackTrace();
+//		}
+//		finally {
+//			repo.getScm().reset();
+//		}
+
+//		writer.write(	
+//			commit.getAuthor().getName(),
+//			commit.getAuthor().getEmail()
+//		);
 
 	}
 
--- Artifact_SA/src/main/java/Group_SA/Artifact_SA/RefactorStudy.java
+++ Artifact_SA/src/main/java/Group_SA/Artifact_SA/RefactorStudy.java
@@ -1,5 +1,8 @@
 package Group_SA.Artifact_SA;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
 import org.repodriller.RepoDriller;
 import org.repodriller.RepositoryMining;
 import org.repodriller.Study;
@@ -10,21 +13,34 @@ import org.repodriller.scm.GitRemoteRepository;
 
 public class RefactorStudy implements Study 
 {
+	public static final String repository = "https://github.com/SonarSource/sonarqube.git"; //"https://github.com/tuplejump/MapDB.git"
+	public static final String repo_name = "sonarqube";
+	
     public static void main(String[] args) {
 		new RepoDriller().start(new RefactorStudy());
 	}
 	
 	@Override
 	public void execute() {
-		new RepositoryMining()
-			.in(
-					GitRemoteRepository
-					.hostedOn("https://github.com/tuplejump/MapDB.git")
-					.inTempDir("/temp")
-					.buildAsSCMRepository()
-					)
-			.through(Commits.all())
-			.process(new DevelopersVisitor(), new CSVFile("../dataMAPDB.csv"))
-			.mine();
+		try {
+			SimpleDateFormat sdf = new SimpleDateFormat("yyyy:mm:dd");
+			Calendar from = Calendar.getInstance();
+			from.setTime(sdf.parse("2017:05:01"));
+			Calendar to = Calendar.getInstance();
+			to.setTime(sdf.parse("2018:06:01"));
+			new RepositoryMining()
+				.in(
+						GitRemoteRepository
+						.hostedOn(repository)
+						.inTempDir("/temp")
+						.buildAsSCMRepository()
+						)
+				.through(Commits.betweenDates(from, to))
+				.process(new DevelopersVisitor(), new CSVFile("../dataMAPDB.csv"))
+				.mine();
+			}
+		catch(Exception e) {
+			//this really shouldnt happen but well, compile errors on the sdf parse without try/catch..
+		}
 	}
 }
