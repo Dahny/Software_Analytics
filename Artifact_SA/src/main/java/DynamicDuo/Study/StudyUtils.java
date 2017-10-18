b76863e9b702884e70008b3c72df49c10c8ab45b
Danny Plenge <dannyplenge@gmail.com>
2017-10-19 00:39:16 +0200

    Changes nog zonder repodriller build

--- Artifact_SA/src/main/java/DynamicDuo/Study/StudyUtils.java
+++ Artifact_SA/src/main/java/DynamicDuo/Study/StudyUtils.java
@@ -1,11 +1,10 @@
 package DynamicDuo.Study;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
-import java.nio.file.LinkOption;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.nio.file.StandardOpenOption;
 import java.util.Optional;
 import java.util.stream.Stream;
 
@@ -23,9 +22,9 @@ public class StudyUtils {
 		}
 		return test;
 	}
-	
+
 	public static boolean isMethod(String entityName) {
-		//only a function namespace will contain brackets
+		// only a function namespace will contain brackets
 		return entityName.contains("(") && entityName.contains(")");
 	}
 
@@ -34,19 +33,24 @@ public class StudyUtils {
 		String[] parts = prodName.toString().toLowerCase().split("\\\\");
 
 		for (String part : parts) {
-			System.out.println(part);
 			main = main || part.equals("main");
 		}
 
 		StringBuilder path = new StringBuilder();
 		if (main) {
+			boolean notFound = true;
+
 			for (String part : parts) {
 				path.append(part);
-				if (path.toString().endsWith("src")) {
+				if (path.toString().endsWith("src") && fileExists(path.toString() + "\\test")) {
 					path.append("\\test");
+					notFound = false;
 					break;
 				}
 				path.append("\\");
+			}
+			if (notFound) {
+				return "no testfile found";
 			}
 		}
 		String name = parts[parts.length - 1];
@@ -54,13 +58,17 @@ public class StudyUtils {
 		return findTestFile(path.toString(), name);
 	}
 
-	public static String findTestFile(String path, String prodName) {
-		
+	public static boolean fileExists(String path) {
+		File f = new File(path);
+		return f.exists();
+	}
+
+	private static String findTestFile(String path, String prodName) {
+		String testPath = "no testfile found";
 		try (Stream<Path> paths = Files.walk(Paths.get(path))) {
 			Optional<Path> opt = paths
 					.filter(p -> p.getFileName().toString().toLowerCase().equals(prodName + "test.java")).findFirst();
-			String testPath = "no testfile found";
-			
+
 			if (opt.isPresent()) {
 				testPath = opt.get().toString();
 			}
@@ -69,55 +77,74 @@ public class StudyUtils {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		return "no testfile found";
+
+		return testPath;
 	}
-	
+
+	/**
+	 * @param loc
+	 *            Line of codes
+	 * @param wmc
+	 *            Weight method class
+	 * @param hsv
+	 *            HalsteadValue
+	 * @return maintainabilityIndex
+	 */
 	public static int getMaintainabilityIndex(int loc, int wmc, double hsv) {
-		return (int)Math.round(Math.max(0, (171 - (5.2*Math.log(hsv)) - (0.23*wmc) -  (16.2*Math.log(loc)))*100 / 171));
+		return (int) Math
+				.round(Math.max(0, (171 - (5.2 * Math.log(hsv)) - (0.23 * wmc) - (16.2 * Math.log(loc))) * 100 / 171));
 	}
-	
+
 	public static CKNumber getMetricsForFile(String pathToFile) {
 		CK ck = new CK();
 		String path = StudyUtils.getPathToContainingFolder(pathToFile);
 		String fileName = StudyUtils.getFileNameFromPath(pathToFile);
 		System.out.println("CALCULATING CK FOR FILES ON: " + path);
 		CKReport report = ck.calculate(path);
-    	return report.get(path.toString()+fileName);
+		return report.get(path.toString() + fileName);
 	}
-	
+
 	public static String getPathToContainingFolder(String pathToFile) {
-		StringBuilder path = new StringBuilder(StudyConstants.Repo_Path_Absolute + StudyConstants.Repo_Name +"\\"); 
-    	String[] parts = pathToFile.split("/", -1);
-    	parts[parts.length-1] = "";
-    	for(String part : parts) {
-    		path.append(part).append("\\");
-    	}
-    	path.delete(path.length() - 2, path.length() - 1);
-    	return path.toString();
+		StringBuilder path = new StringBuilder(StudyConstants.Repo_Path_Absolute + StudyConstants.Repo_Name + "\\");
+		String[] parts = pathToFile.split("/", -1);
+		parts[parts.length - 1] = "";
+		for (String part : parts) {
+			path.append(part).append("\\");
+		}
+		path.delete(path.length() - 2, path.length() - 1);
+		return path.toString();
 	}
 
 	public static String getFileNameFromPath(String pathToFile) {
-    	String[] parts = pathToFile.split("/", -1);
-    	return parts[parts.length-1];
+		String[] parts = pathToFile.split("/", -1);
+		return parts[parts.length - 1];
 	}
-	
+
 	public static String getAbsolutePathToFile(String pathToFile) {
 		return getPathToContainingFolder(pathToFile) + getFileNameFromPath(pathToFile);
 	}
-	
+
 	public static String getClassPathFromNameSpace(String namespace) {
 		String reverseClassNamespace = new StringBuffer(namespace).reverse().toString().split(".", 1)[0];
 		String classNameSpace = new StringBuffer(reverseClassNamespace).reverse().toString();
-		return classNameSpace.replace('.', '/')+".java";
+		return classNameSpace.replace('.', '/') + ".java";
 	}
-	
+
 	public static String getClassPathFromRefactoringDescription(String description) {
-		String[] refStringParts = description.split("in class ");//index 1 should contain the class namespace
-		if(refStringParts.length > 1) {
+		String[] refStringParts = description.split("in class ");// index 1
+																	// should
+																	// contain
+																	// the class
+																	// namespace
+		if (refStringParts.length > 1) {
 			return getClassPathFromNameSpace(refStringParts[1]);
 		}
 		return null;
 	}
-	
+
+	public static String readFile(String path) throws IOException {
+		byte[] encoded = Files.readAllBytes(Paths.get(path));
+		return new String(encoded, "UTF-8");
+	}
+
 }
--- Artifact_SA/src/main/java/DynamicDuo/Visitors/MonthlyMetricVisitor.java
+++ Artifact_SA/src/main/java/DynamicDuo/Visitors/MonthlyMetricVisitor.java
@@ -1,55 +1,66 @@
 package DynamicDuo.Visitors;
 
-import java.util.List;
-
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.revwalk.RevCommit;
-import org.refactoringminer.api.GitHistoryRefactoringMiner;
-import org.refactoringminer.api.GitService;
-import org.refactoringminer.api.Refactoring;
-import org.refactoringminer.api.RefactoringHandler;
-import org.refactoringminer.rm1.GitHistoryRefactoringMinerImpl;
-import org.refactoringminer.util.GitServiceImpl;
 import org.repodriller.domain.Commit;
-import org.repodriller.domain.Modification;
-import org.repodriller.domain.ModificationType;
 import org.repodriller.persistence.PersistenceMechanism;
 import org.repodriller.scm.CommitVisitor;
 import org.repodriller.scm.SCMRepository;
 import com.github.mauricioaniche.ck.CK;
+import com.github.mauricioaniche.ck.CKNumber;
 import com.github.mauricioaniche.ck.CKReport;
 
-import DynamicDuo.RefactoringUtils.IdentifiedRefactorCommitsHolder;
-import DynamicDuo.Study.RefactorStudy;
+import DynamicDuo.RefactoringUtils.HalsteadExtractor;
 import DynamicDuo.Study.StudyConstants;
+import DynamicDuo.Study.StudyUtils;
+import DynamicDuo.Utils.CsvFileWriter;
 
 public class MonthlyMetricVisitor implements CommitVisitor {
-	
+
 	@Override
-	public void process(SCMRepository repo, Commit commit, PersistenceMechanism writer) {		
+	public void process(SCMRepository repo, Commit commit, PersistenceMechanism writer) {
 		try {
 			CK ck = new CK();
-			StringBuilder path = new StringBuilder(StudyConstants.Repo_Path_Absolute); 
+			StringBuilder path = new StringBuilder(StudyConstants.Repo_Path_Absolute);
 			path.append(StudyConstants.Repo_Name);
-			
+
 			repo.getScm().checkout(commit.getHash());
 			CKReport report = ck.calculate(path.toString());
-			//TODO: 
-			//parse report
-			//CALCULATE MAINAINABILITY
-			//find test-prod pairs
-			//write pairs to file
-			//write pairs+metrics to file
-//			writer.write();
-		} 
-		catch(Exception e) {
+
+			// parse report
+			for (CKNumber ckn : report.all()) {
+				int loc = ckn.getLoc();
+				int wmc = ckn.getWmc();
+				double hsv = HalsteadExtractor.calculateHalsteadVolume(StudyUtils.readFile(ckn.getFile()));
+
+				// CALCULATE MAINAINABILITY
+				int mtnIndex = StudyUtils.getMaintainabilityIndex(loc, wmc, hsv);
+
+				// find test-prod pairs
+				String fileType = "TestFile";
+				if (!StudyUtils.isTestClass(ckn.getFile())) {
+					fileType = "ProductionFile";
+					String pair = StudyUtils.findTestPair(ckn.getFile());
+
+					if (!pair.equals("no testfile found")) {
+						// write pairs to file
+						CsvFileWriter.writeLineToCsv(
+								"C:\\Users\\Dplen\\Documents\\Software_Analytics\\Artifact_SA\\data\\PairDataSonarQube.csv",
+								ckn.getFile() + ", " + pair);
+					}
+				}
+
+				// Write -> fileType, absolute file path, MaintainabilityIndex,
+				// [ALL CK METRICS].
+				writer.write(fileType, ckn.getFile(), mtnIndex, ckn.getCbo(), ckn.getDit(), ckn.getNoc(), ckn.getNof(),
+						ckn.getNopf(), ckn.getNosf(), ckn.getNom(), ckn.getNopm(), ckn.getNosm(), ckn.getNosi(),
+						ckn.getRfc(), ckn.getWmc(), ckn.getLoc(), ckn.getLcom());
+
+			}
+		} catch (Exception e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
+			//Thread.
 			repo.getScm().reset();
 		}
-
-
 
 	}
 
